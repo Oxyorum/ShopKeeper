579b16750d3ae629135d1cf075b064eb9ac0970e
Oxyorum <oxyorum@gmail.com>
2016-07-20 16:44:15 -0400

    First commit.

---
+++ src/main/java/io/github/oxyorum/shopkeeper/ShopKeeper.java
@@ -0,0 +1,8 @@
+package io.github.oxyorum.shopkeeper;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "shopkeeper", name = "ShopKeeper", version="1.0")
+public class ShopKeeper {
+	
+}
